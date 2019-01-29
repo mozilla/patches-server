da8012a09bbd562db6e385c2c3bbd0651112c73b
Emma Rose <emma.arcadia.rose@gmail.com>
2019-01-28 21:17:36 -0500

    Require that a cache be populated with platforms to support

--- patches/test/patches/server/cache_test.exs
+++ patches/test/patches/server/cache_test.exs
@@ -4,15 +4,11 @@ defmodule Patches.Server.CacheTest do
 
   alias Patches.Server.Cache
 
-  test "can be constructed with a starting list of platforms to support" do
+  test "must be constructed with a starting list of platforms to support" do
     cache1 = Cache.init(["platform1", "platform2"])
-    cache2 = Cache.init()
 
     assert Map.has_key?(cache1, "platform1")
     assert Map.has_key?(cache1, "platform2")
-
-    assert not Map.has_key?(cache2, "platform1")
-    assert not Map.has_key?(cache2, "platform2")
   end
 
   test "never exceeds maximum capacity" do
