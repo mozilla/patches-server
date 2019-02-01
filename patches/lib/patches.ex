0a9b2f1eb2f2522172895fd9935932d838c2c752
Emma Rose <emma.arcadia.rose@gmail.com>
2019-02-01 17:56:40 -0500

    mix new patches

---
+++ patches/lib/patches.ex
@@ -0,0 +1,18 @@
+defmodule Patches do
+  @moduledoc """
+  Documentation for Patches.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Patches.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ patches/lib/patches/application.ex
@@ -0,0 +1,20 @@
+defmodule Patches.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Patches.Worker.start_link(arg)
+      # {Patches.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Patches.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
