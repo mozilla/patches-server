a9a92af60e8334b0f4ad513e48eceec2aabd1770
Emma Rose <emma.arcadia.rose@gmail.com>
2019-02-03 18:25:47 -0500

    Implement an init function for CacheWindow. Window over a List should not add an extra item

--- patches/lib/patches/cache_window.ex
+++ patches/lib/patches/cache_window.ex
@@ -9,11 +9,26 @@ defimpl Window, for: List do
   def view(collection, start_index, length) do
     collection
     |> Enum.drop(start_index)
-    |> Enum.take(length - start_index + 1)
+    |> Enum.take(length - start_index)
   end
 end
 
 defmodule Patches.CacheWindow do
   @moduledoc """
+  Mantains a view over a collection.
   """
+
+  @doc """
+  Initialize a representation of a sliding window over a collection.
+
+  The collection must implement the `Window` protocol.
+  """
+  def init(collection, start_size) when is_integer(start_size) do
+    %{
+      collection: collection,
+      view: Window.view(collection, 0, start_size),
+      start_index: 0,
+      length: start_size,
+    }
+  end
 end
--- patches/test/patches/cache_window_test.exs
+++ patches/test/patches/cache_window_test.exs
@@ -2,8 +2,16 @@ defmodule Patches.CacheWindowTest do
   use ExUnit.Case
   doctest Patches.CacheWindow
 
-  alias Patches.CacheWindow, as: CW
+  alias Patches.CacheWindow
 
   test "initializes to provide a view over a given number of elements" do
+    list =
+      [1,2,3,4,5,6,7]
+
+    %{ collection: coll, view: view } =
+      CacheWindow.init(list, 3)
+
+    assert coll == list
+    assert view == Enum.take(list, 3)
   end
 end
