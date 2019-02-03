50ad3693b7e2e6d075d3d44ed3a4210c5f067083
Emma Rose <emma.arcadia.rose@gmail.com>
2019-02-03 18:13:37 -0500

    Removed irrelevant test

--- patches/lib/patches/cache_window.ex
+++ patches/lib/patches/cache_window.ex
@@ -5,15 +5,15 @@ defprotocol Window do
   def view(collection, start_index, length)
 end
 
-defmodule Patches.CacheWindow do
-  @moduledoc """
-  """
-end
-
 defimpl Window, for: List do
   def view(collection, start_index, length) do
     collection
     |> Enum.drop(start_index)
     |> Enum.take(length - start_index + 1)
   end
+end
+
+defmodule Patches.CacheWindow do
+  @moduledoc """
+  """
 end
--- patches/test/patches/cache_window_test.exs
+++ patches/test/patches/cache_window_test.exs
@@ -4,14 +4,6 @@ defmodule Patches.CacheWindowTest do
 
   alias Patches.CacheWindow, as: CW
 
-  test "supports any type that implements the `Window` protocol" do
-    window =
-      [1,5,10,23,2,15,6,9]
-      |> Window.view(1, 5)
-
-    assert window == [5,10,23,2,15]
-  end
-
-  test "initializes to provide over a given number of elements" do
+  test "initializes to provide a view over a given number of elements" do
   end
 end
