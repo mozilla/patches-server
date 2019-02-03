da265fde4dc4356743bbd26b7265a2d2c0db2614
Emma Rose <emma.arcadia.rose@gmail.com>
2019-02-03 18:42:42 -0500

    Add support to update a CacheWindow's collection and view parameters

--- patches/lib/patches/cache_window.ex
+++ patches/lib/patches/cache_window.ex
@@ -31,4 +31,21 @@ defmodule Patches.CacheWindow do
       length: start_size,
     }
   end
+
+  @doc """
+  Apply a function to update the underlying collection managed by the
+  `CacheWindow` as well as to compute a new start index and length for the
+  view over it.
+  """
+  def update(cache, update_fn) when is_function(update_fn) do
+    {new_coll, new_start, new_len} =
+      update_fn.(cache.collection, cache.start_index, cache.length)
+
+    %{
+      collection: new_coll,
+      view: Window.view(new_coll, new_start, new_len),
+      start_index: new_start,
+      length: new_len,
+    }
+  end
 end
--- patches/test/patches/cache_window_test.exs
+++ patches/test/patches/cache_window_test.exs
@@ -14,4 +14,33 @@ defmodule Patches.CacheWindowTest do
     assert coll == list
     assert view == Enum.take(list, 3)
   end
+
+  test "can update the collection" do
+    list =
+      [1,2,3,4,5,6,7]
+
+    %{ collection: coll } =
+      CacheWindow.init(list, 3)
+      |> CacheWindow.update(fn (collection, start, len) ->
+        new_coll =
+          Enum.filter(collection, fn n -> Integer.mod(n, 2) == 0 end)
+
+        {new_coll, start, len}
+      end)
+
+    assert coll == [2,4,6]
+  end
+
+  test "can update the window" do
+    list =
+      [1,2,3,4,5,6,7]
+
+    %{ view: view } =
+      CacheWindow.init(list, 3)
+      |> CacheWindow.update(fn (collection, start, len) ->
+        {collection, start, len + 2}
+      end)
+
+    assert view == [1,2,3,4,5]
+  end
 end
