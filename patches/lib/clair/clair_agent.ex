50ebbfc6b5c1c943ba1818d9a05cade53f5c22ac
Emma Rose <emma.arcadia.rose@gmail.com>
2019-02-05 16:42:54 -0500

    More agent tests and a state machine diagram

--- patches/lib/clair/clair_agent.ex
+++ patches/lib/clair/clair_agent.ex
@@ -1,6 +1,30 @@
 defmodule Clair.Agent do
   @moduledoc """
   An asynchronous interface to a Clair-backed vulnerability source.
+  
+  ## States
+  
+  1. `:not_ready` indicates that no vulnerabilities are available yet.
+  2. `{:error, reason}` indicates that an error has been encountered.
+  3. `{:ok, vulns}` indicates that vulnerabilities are available.
+
+  ```
+                  
+    ,---,  a   ,---, .---, 
+    | 1 | ---> | 2 |<    |d
+    '---'      '---' '---'
+     /  ^
+   b|   |c
+    v  /
+    ,---,
+    | 3 |
+    '---'
+  ```
+
+  * Transition `a` occurs when an error is encounterd tring to fetch vulns.
+  * Transition `b` occurs when vulns are fetched successfully.
+  * Transition `c` occurs when vulns are retrieved from the agent.
+  * Transition `d` occurs to keep the agent in the error state once entered.
   """
 
   use Agent
@@ -9,12 +33,6 @@ defmodule Clair.Agent do
 
   @doc """
   Start a link to an `Agent` containing a `Clair` configuration.
-
-  ## States
-  
-  1. `:not_ready` indicates that no vulnerabilities are available yet.
-  2. `{:error, reason}` indicates that an error has been encountered.
-  3. `{:ok, vulns}` indicates that vulnerabilities are available.
   """
   def start_link(clair_config) do
     init =
--- patches/test/clair/clair_agent_test.exs
+++ patches/test/clair/clair_agent_test.exs
@@ -4,7 +4,7 @@ defmodule Clair.AgentTest do
 
   setup do
     config =
-      Clair.init("http://127.0.0.1:6060", "ubuntu:18.04", 32)
+      Clair.init("http://127.0.0.1:6060", "ubuntu:18.04", 32, Clair.HttpSuccessStub)
 
     {:ok, pid} =
       Clair.Agent.start_link(config)
@@ -17,5 +17,23 @@ defmodule Clair.AgentTest do
 
   test "asserts that no vulns are loaded upon initialization" do
     assert not Clair.Agent.has_vulns?()
+  end
+
+  test "vulnerabilities can be fetched asynchronously" do
+    Clair.Agent.fetch(fn -> send(self(), :ready) end)
+
+    receive do
+      :ready ->
+        assert Clair.Agent.has_vulns?()
+
+      _ ->
+        assert false
+    end
+
+    assert Enum.count(Clair.Agent.vulnerabilities()) == 2
+  end
+
+  test "no vulnerabilities present before fetching" do
+    assert Enum.count(Clair.Agent.vulnerabilities()) == 0
   end
 end
