9b20f9f52f8b7873a52575666211be97be8637f6
Emma Rose <emma.arcadia.rose@gmail.com>
2019-02-06 17:23:34 -0500

    Describe all the functions the StreamManager module needs to implement

--- patches/lib/patches/stream_manager.ex
+++ patches/lib/patches/stream_manager.ex
@@ -32,7 +32,53 @@ defmodule Patches.StreamManager do
   @doc """
   Start and link the `StreamManager`.
   """
-  def start_link() do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  def start_link(config) do
+    init =
+      %{
+        config: config,
+        caches: %{},
+      }
+
+    Agent.start_link(fn -> init end, name: __MODULE__)
+  end
+
+  @doc """
+  Remove any currently managed sessions and replace them with a new collection
+  thereof.
+
+  ## Arguments
+  
+  1. `sessions` is a list of `Patches.Server.Session`.
+  2. `create_cache` is a function that, given a platform string, constructs a
+  `Patches.CacheWindow`.
+  """
+  def manage(sessions, create_cache) when is_function(create_cache) do
+  end
+
+  @doc """
+  Retrieve information about the states of sessions belonging to scanners
+  running a scan for a particular platform.
+  """
+  def sessions(platform) when is_binary(platform) do
+  end
+
+  @doc """
+  Retrieve data from the cache being managed for a particular scanner session.
+  """
+  def retrieve(session_id) when is_binary(session_id) do
+  end
+
+  @doc """
+  Determine if all scanners whose sessions are being managed by the
+  `StreamManager` are complete.
+  """
+  def all_sessions_complete?() do
+  end
+
+  @doc """
+  Determine if all of the sessions for scanners retrieving vulnerabilities for a
+  particular platform are complete.
+  """
+  def all_sessions_complete?(platform) when is_binary(platform) do
   end
 end
