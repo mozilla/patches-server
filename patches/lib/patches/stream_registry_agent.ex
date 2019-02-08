e7589c5941fac6a9dcf92ba2d07e43e05c0348ed
Emma Rose <emrose@mozilla.com>
2019-02-08 16:56:41 -0500

    Implemented functions to register new sessions and lookup values in a cache for a session

--- patches/lib/patches/stream_registry_agent.ex
+++ patches/lib/patches/stream_registry_agent.ex
@@ -11,7 +11,7 @@ defmodule Patches.StreamRegistry.Agent.Config do
 
   defstruct(
     max_window_length: 8000,
-    lookup_limit: 128,
+    lookup_limit: 128
   )
 end
 
@@ -20,6 +20,8 @@ defmodule Patches.StreamRegistry.Agent do
   """
 
   use Agent
+  
+  alias Patches.StreamRegistry, as: Registry
 
   @doc """
   Start a link to an `Agent` maintaining a given `StreamRegistry`.
@@ -32,5 +34,39 @@ defmodule Patches.StreamRegistry.Agent do
       }
 
     Agent.start_link(fn -> init end, name: __MODULE__)
+  end
+
+  def register_sessions(platform, collection, sessions) do
+    %{ max_window_length: limit } =
+      Agent.get(__MODULE__, fn %{ config: cfg } -> cfg end)
+      
+    register_sessions(platform, collection, sessions, limit)
+  end
+  
+  def register_sessions(platform, collection, sessions, window_length) do
+    Agent.update(__MODULE__, fn state=%{ registry: reg } ->
+      new_registry =
+        Registry.register_sessions(
+          reg,
+          platform: platform,
+          collection: collection,
+          sessions: sessions,
+          window_length: window_length)
+
+      %{ state | registry: new_registry }
+    end)
+  end
+
+  def retrieve(session_id) do
+    %{ lookup_limit: limit } =
+      Agent.get(__MODULE__, fn %{ config: cfg } -> cfg end)
+    
+    retrieve(session_id, limit)
+  end
+
+  def retrieve(session_id, limit) do
+    Agent.get(__MODULE__, fn %{ registry: reg } ->
+      Registry.cache_lookup(reg, session_id, limit)
+    end)
   end
 end
--- patches/test/patches/stream_registry_agent_test.exs
+++ patches/test/patches/stream_registry_agent_test.exs
@@ -2,6 +2,7 @@ defmodule Patches.StreamRegistry.AgentTest do
   use ExUnit.Case, async: true
   doctest Patches.StreamRegistry.Agent
 
+  alias Patches.StreamRegistry, as: Registry
   alias Patches.StreamRegistry.Agent, as: SRAgent
   alias Patches.StreamRegistry.Agent.Config
 
@@ -19,5 +20,61 @@ defmodule Patches.StreamRegistry.AgentTest do
       config: config,
       pid: pid,
     }
+  end
+
+  test "can instruct to activated sessions" do
+    sessions =
+      [
+        %Patches.Server.Session{ platform: "discarded", id: "test1" },
+        %Patches.Server.Session{ platform: "discarded", id: "test2" },
+      ]
+
+    assert SRAgent.register_sessions("ubuntu:18.04", [1,2,3,4,5], sessions) == :ok
+  end
+
+  test "can retrieve values from an appropriate cache" do
+    sessions1 =
+      [
+        %Patches.Server.Session{ platform: "discarded", id: "test1" },
+        %Patches.Server.Session{ platform: "discarded", id: "test2" },
+      ]
+    
+    sessions2 =
+      [
+        %Patches.Server.Session{ platform: "discarded", id: "test3" },
+        %Patches.Server.Session{ platform: "discarded", id: "test4" },
+      ]
+    
+    SRAgent.register_sessions("ubuntu:18.04", [1,2,3,4,5], sessions1)
+    SRAgent.register_sessions("alpine:3.4", [6,7,8], sessions2)
+
+    assert SRAgent.retrieve("test1") == [1,2,3,4,5]
+    assert SRAgent.retrieve("test4") == [6,7,8]
+  end
+
+  test "can limit the maximum window size over a collection when sessions are registered" do
+    sessions =
+      [
+        %Patches.Server.Session{ platform: "discarded", id: "test1" },
+        %Patches.Server.Session{ platform: "discarded", id: "test2" },
+      ]
+
+    SRAgent.register_sessions("ubuntu:18.04", [1,2,3,4,5], sessions, 3)
+
+    assert SRAgent.retrieve("test1") == [1,2,3]
+    assert SRAgent.retrieve("test2") == [1,2,3]
+  end
+
+  test "can limit the number of values retrieved from a cache" do
+    sessions =
+      [
+        %Patches.Server.Session{ platform: "discarded", id: "test1" },
+        %Patches.Server.Session{ platform: "discarded", id: "test2" },
+      ]
+
+    SRAgent.register_sessions("ubuntu:18.04", [1,2,3,4,5], sessions)
+
+    assert SRAgent.retrieve("test1", 1) == [1]
+    assert SRAgent.retrieve("test2", 2) == [1,2]
   end
 end
