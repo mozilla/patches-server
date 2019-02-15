ecd2fb4f07c41d9f3d4af54d8a147ddde90d77e1
Emma Rose <emrose@mozilla.com>
2019-02-15 16:02:14 -0500

    Implemented a stateful Agent to simply track sessions and apply configured limits on queue and active session lists

--- patches/lib/patches/server_agent.ex
+++ patches/lib/patches/server_agent.ex
@@ -18,19 +18,23 @@ defmodule Patches.Server.Agent.Config do
 
   defstruct(
     max_active_sessions: 128,
-    max_queued_sessions: 1024,
-    active_session_timeout_seconds: 10 * 60,
-    queued_session_timeout_seconds: 5 * 60,
-    max_vuln_cache_size: 1024,
+    max_queued_sessions: 1024
   )
 end
 
 defmodule Patches.Server.Agent do
   @moduledoc """
-  An agent that manages a `Patches.Server` state.
+  An agent with the responsibility of maintaining the state of active and
+  queued sessions waiting to be handled.  It has an implicit dependency on the
+  `Patches.StreamRegistry.Agent`.
   """
 
   use Agent
+
+  alias Patches.Server
+  alias Patches.Server.Agent.Config
+
+  def start_link(config \\ %Config{})
 
   @doc """
   Start and link a new server agent configured to apply constraints to the state.
@@ -42,6 +46,111 @@ defmodule Patches.Server.Agent do
         state: Patches.Server.init(),
       }
 
-    Agent.start_link(fn -> init end)
+    Agent.start_link(fn -> init end, name: __MODULE__)
+  end
+
+  @doc """
+  Enqueue a new session for a scanner wishing to retrieve vulns for a specific platform.
+
+  Returns
+
+      {:ok, session_id}
+
+  if there is room in the queue, where `session_id` is the identifier generated for the
+  new session. If there is not room in the queue, then
+
+      {:error, :queue_full}
+
+  is returned instead.
+  """
+  def queue_session(scanning: platform) when is_binary(platform) do
+    Agent.get_and_update(__MODULE__, fn %{ state: state, config: config } ->
+      new_session_id =
+        generate_id(fn id -> not Map.has_key?(state.queued_sessions, id) end)
+
+      {to_caller, new_state} =
+        if Enum.count(state.queued_sessions) <= config.max_queued_sessions do
+          {
+            {:ok, new_session_id},
+            Server.queue_session(state, platform, new_session_id),
+          }
+        else
+          {
+            {:error, :queue_full},
+            state,
+          }
+        end
+
+      {to_caller, %{ state: new_state, config: config }}
+    end)
+  end
+
+  @doc """
+  Activate up to a configured number of queued sessions, replacing any that were
+  active at the time of calling.
+
+  Returns the number of sessions that were activated.
+  """
+  def activate_sessions() do
+    Agent.get_and_update(__MODULE__, fn %{ state: state, config: config } ->
+      limit =
+        Enum.min([
+          config.max_active_sessions,
+          Enum.count(state.queued_sessions),
+        ])
+
+      {activated, new_state} =
+        Server.activate_sessions(state, limit)
+      
+      {activated, %{ state: new_state, config: config }}
+    end)
+  end
+
+  @doc """
+  Retrieve a list of all sessions currently active.
+  """
+  def active() do
+    Agent.get(__MODULE__, fn %{ state: %{ active_sessions: a } } ->
+      a
+      |> Enum.into([])
+      |> Enum.map(fn {_id, session} -> session end)
+    end)
+  end
+
+  @doc """
+  Retrieve a list of all sessions currently queued.
+  """
+  def queued() do
+    Agent.get(__MODULE__, fn %{ state: %{ queued_sessions: q } } ->
+      q
+      |> Enum.into([])
+      |> Enum.map(fn {_id, session} -> session end)
+    end)
+  end
+  
+  @doc """
+  Generate a new random hex ID of a particular `length`.
+
+  ## Arguments
+
+    1 (optional) The number of bytes of hex to generate. Defaults to `32`.
+    2. The `unqiue` argument must be a predicate function that, given a
+    generated ID, returns true if the ID is unique or else false.
+
+  ## Returns
+
+  A hex string of the specified `length`.
+  """
+  def generate_id(length \\ 32, unique) when is_function(unique) do
+    id =
+      1..length
+      |> Enum.map(fn _n -> String.at("abcdef0123456789", :rand.uniform(15)) end)
+      |> Enum.reduce("", fn (char, str) -> str <> char end)
+
+    if unique.(id) do
+      id
+    else
+      generate_id(length, unique)
+    end
   end
 end
--- patches/test/patches/server_agent_test.exs
+++ patches/test/patches/server_agent_test.exs
@@ -2,24 +2,49 @@ defmodule Patches.Server.AgentTest do
   use ExUnit.Case, async: true
   doctest Patches.Server.Agent
 
+  alias Patches.Server.Agent, as: ServerAgent
   alias Patches.Server.Agent.Config, as: AgentConfig
 
   setup do
     config =
       %AgentConfig{
-        max_active_sessions: 3,
-        max_queued_sessions: 5,
-        active_session_timeout_seconds: 3,
-        queued_session_timeout_seconds: 10,
-        max_vuln_cache_size: 3,
+        max_active_sessions: 1,
+        max_queued_sessions: 2,
       }
 
     {:ok, pid} =
-      Patches.Server.Agent.start_link()
+      ServerAgent.start_link(config)
 
     %{
       config: config,
-      pii: pid,
+      server: pid,
     }
+  end
+
+  test "enforces a limit on the number of sessions to queue" do
+    {:ok, _id} =
+      ServerAgent.queue_session(scanning: "ubuntu:18.04")
+
+    {:ok, _id} =
+      ServerAgent.queue_session(scanning: "alpine:3.4")
+    
+    {:ok, _id} =
+      ServerAgent.queue_session(scanning: "debian:unstable")
+
+    {:error, :queue_full} =
+      ServerAgent.queue_session(scanning: "ubuntu:18.04")
+  end
+
+  test "enforces a limit on the number of sessions to activate" do
+    {:ok, _id} =
+      ServerAgent.queue_session(scanning: "ubuntu:18.04")
+
+    {:ok, _id} =
+      ServerAgent.queue_session(scanning: "alpine:3.4")
+
+    activated =
+      ServerAgent.activate_sessions()
+
+    assert activated == 1
   end
 end
