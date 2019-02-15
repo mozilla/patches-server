c105f5ae0f6ea15971ef07409661dbc621396f64
Emma Rose <emrose@mozilla.com>
2019-02-15 13:04:15 -0500

    Fixed up tests to account for Patches.Server API changes

--- patches/lib/patches/server.ex
+++ patches/lib/patches/server.ex
@@ -1,17 +1,3 @@
-defmodule Patches.Server.Config do
-  @moduledoc """
-  A structure containing the data that a user can supply to configure the
-  behaviour of a `Patches.Server`.
-  """
-
-  defstruct(
-    max_active_sessions: 128,
-    max_queued_sessions: 1024,
-    active_session_timeout_seconds: 10 * 60,
-    queued_session_timeout_seconds: 5 * 60
-  )
-end
-
 defmodule Patches.Server.Session do
   @moduledoc """
   A structure representing a scanner session.
@@ -31,11 +17,10 @@ defmodule Patches.Server do
   """
 
   @doc """
-  Construt a new `Patches.Server` with a given or default configuration.
+  Construt an empty `Patches.Server`.
   """
-  def init(config \\ %Patches.Server.Config{}) do
+  def init() do
     %{
-      config: config,
       active_sessions: %{},
       queued_sessions: %{},
     }
@@ -44,78 +29,32 @@ defmodule Patches.Server do
   @doc """
   Enqueue a new session for a scanner wishing to retrieve information about
   vulnerabilities affecting packages for a particular platform.
-
-  If the server is already maintaining the maximum configured number of queued
-  sessions allowed, this function returns
-
-      {:error, :queue_full, server}
-
-  with `server` containing the current state of the server. Otherwise, it
-  returns
-
-      {:ok, id, server}
-
-  with `server.queued_sessions` containing a new key, `id`, corresponding to a
-  new session value.
   """
-  def queue_session(server, platform) when is_binary(platform) do
-    if Enum.count(server.queued_sessions) < server.config.max_queued_sessions do
-      new_session_id =
-        generate_id(fn id -> not Map.has_key?(server.queued_sessions, id) end)
+  def queue_session(server, platform, session_id) do
+    new_session =
+      %Patches.Server.Session{
+        platform: platform,
+        id: session_id,
+        created_at: Time.utc_now(),
+      }
 
-      new_session =
-        %Patches.Server.Session{
-          platform: platform,
-          id: new_session_id,
-          created_at: Time.utc_now(),
-        }
-
-      new_server =
-        %{
-          server | queued_sessions: Map.put(
-            server.queued_sessions,
-            new_session_id,
-            new_session)
-        }
-
-      {:ok, new_session_id, new_server}
-    else
-      {:error, :queue_full, server}
-    end
+    %{
+      server |
+      queued_sessions: Map.put(server.queued_sessions, session_id, new_session)
+    }
   end
 
   @doc """
-  Move sessions fromm the server's `queued_sessions` collection into its
-  `active_sessions` collection.
+  Take at most `limit` sessions from the waiting queue and put them in the active set.
+  It returns
 
-  Returns
+    {activated, new_server}
 
-      {activated, server}
-
-  with `activated` being the number of sessions that ended up being activated
-  and `server` being the new server state.
-
-  Defaults to activating the configured `max_active_sessions` number of
-  sessions.
+  where
+    * `activated` is the number of sessions that were actually activated, and
+    * `new_server` is the new state of the serer with both queues adjusted.
   """
-  def activate_sessions(server) do
-    activate_sessions(server, server.config.max_active_sessions)
-  end
-
-  @doc """
-  See `activate_sessions/1`.
-  """
-  def activate_sessions(server, num) when is_integer(num) do
-    currently_active =
-      Enum.count(server.active_sessions)
-
-    to_take =
-      Enum.min([
-        num,
-        Enum.count(server.queued_sessions),
-        server.config.max_active_sessions - currently_active,
-      ])
-
+  def activate_sessions(server, limit) when is_integer(limit) do
     sessions_by_created_at =
       server.queued_sessions
       |> Enum.into([])
@@ -123,54 +62,27 @@ defmodule Patches.Server do
 
     active_sessions =
       sessions_by_created_at
-      |> Enum.take(to_take)
+      |> Enum.take(limit)
       |> Enum.into(%{})
-      |> Map.merge(server.active_sessions)
 
     queued_sessions =
       sessions_by_created_at
-      |> Enum.drop(to_take)
+      |> Enum.drop(limit)
       |> Enum.into(%{})
 
-    new_server = %{
-      server |
-      active_sessions: active_sessions,
-      queued_sessions: queued_sessions,
-    }
+    new_server =
+      %{
+        active_sessions: active_sessions,
+        queued_sessions: queued_sessions,
+      }
 
-    {to_take, new_server}
+    {Enum.count(active_sessions), new_server}
   end
 
+  @doc """
+  Remove all active sessions from the server.
+  """
   def terminate_active_sessions(server) do
     %{ server | active_sessions: %{} }
-  end
-
-  @doc """
-  Generate a new random hex ID of a particular length. The `unqiue` argument
-  must be a predicate function that, given a generated ID, returns true if the
-  ID is unique or else false if it is not.
-  """
-  defp generate_id(length \\ 32, unique) do
-    id = genid(length)
-    if unique.(id) do
-      id
-    else
-      generate_id(length, unique)
-    end
-  end
-
-  @doc """
-  Generate a random hex string of a particular length.
-  """
-  defp genid(length) do
-    source = "abcdef0123456789"
-    rand_chars = Enum.map(1..length, fn _n ->
-      source
-      |> String.length
-      |> (fn n -> n - 1 end).()
-      |> :rand.uniform()
-      |> (fn n -> String.at(source, n) end).()
-    end)
-    Enum.reduce(rand_chars, "", fn char, str -> str <> char end)
   end
 end
--- patches/test/patches/server_test.exs
+++ patches/test/patches/server_test.exs
@@ -3,13 +3,11 @@ defmodule Patches.ServerTest do
   doctest Patches.Server
 
   alias Patches.Server
-  alias Patches.Server.Config
-
 
   test "new sessions are queued upon creation" do
-    {:ok, _id, %{ queued_sessions: queued }} =
+    %{ queued_sessions: queued } =
       Server.init()
-      |> Server.queue_session("ubuntu:18.04")
+      |> Server.queue_session("ubuntu:18.04", "testid")
 
     assert Enum.count(queued) == 1
   end
@@ -17,8 +15,8 @@ defmodule Patches.ServerTest do
   test "can register sessions with unique identifiers" do
     %{ queued_sessions: queued } =
       Enum.reduce(0..3, Server.init(), fn (_n, server) ->
-        {:ok, _id, server} =
-          Server.queue_session(server, "ubuntu:18.04")
+        server =
+          Server.queue_session(server, "ubuntu:18.04", "testid")
 
         server
       end)
@@ -31,44 +29,12 @@ defmodule Patches.ServerTest do
     assert Enum.count(ids) == Enum.count(queued)
   end
 
-  test "cannot create more than a maximum number of queued sessions" do
-    config =
-      %Config{ max_queued_sessions: 1 }
-
-    with server <- Server.init(config),
-         {:ok, _id, server} <- Server.queue_session(server, "ubuntu:18.04"),
-         {:error, :queue_full, server} <- Server.queue_session(server, "ubuntu:18.04") do
-      assert Enum.count(server.queued_sessions) == 1
-    end
-  end
-
   test "can replace active sessions with queued sessions" do
     {activated, server} =
-      Enum.reduce(0..2, Server.init(), fn (_n, server) ->
-        {:ok, _id, server} =
-          Server.queue_session(server, "ubuntu:18.04")
-
-        server
+      Enum.reduce(1..3, Server.init(), fn (n, server) ->
+        Server.queue_session(server, "ubuntu:18.04", "testid#{n}")
       end)
       |> Server.activate_sessions(1)
-
-    assert activated == 1
-    assert Enum.count(server.active_sessions) == 1
-    assert Enum.count(server.queued_sessions) == 2
-  end
-
-  test "up to a configured number of sessions can be made active at a time" do
-    config =
-      %Patches.Server.Config{ max_active_sessions: 1 }
-
-    {activated, server} =
-      Enum.reduce(0..2, Server.init(config), fn (_n, server) ->
-        {:ok, _id, server} =
-          Server.queue_session(server, "ubuntu:18.04")
-
-        server
-      end)
-      |> Server.activate_sessions(3)
 
     assert activated == 1
     assert Enum.count(server.active_sessions) == 1
@@ -77,20 +43,19 @@ defmodule Patches.ServerTest do
 
   test "the most recently created sessions are activated first" do
     {_activated, server} =
-      Enum.reduce(0..2, Server.init(), fn (_n, server) ->
-        {:ok, _id, server} =
-          Server.queue_session(server, "ubuntu:18.04")
-
-        server
+      Enum.reduce(1..3, Server.init(), fn (n, server) ->
+        Server.queue_session(server, "ubuntu:18.04", "testid#{n}")
       end)
       |> Server.activate_sessions(1)
 
     [ activated | _rest ] =
       server.active_sessions
+      |> Enum.into([])
       |> Enum.map(fn {_id, session} -> session end)
 
     [ queued1 | [ queued2 | _rest ]] =
       server.queued_sessions
+      |> Enum.into([])
       |> Enum.map(fn {_id, session} -> session end)
 
     assert activated.created_at < queued1.created_at
@@ -100,8 +65,7 @@ defmodule Patches.ServerTest do
   test "can support the full session lifecycle" do
     %{ active_sessions: active } =
       Server.init()
-      |> Server.queue_session("ubuntu:18.04")
-      |> (fn {:ok, _id, server} -> server end).()
+      |> Server.queue_session("ubuntu:18.04", "testid")
       |> Server.activate_sessions(1)
       |> (fn {_activated, server} -> server end).()
       |> Server.terminate_active_sessions()
