4f27b8f4ad675897723554a13fd5cffd5ab7993a
Emma Rose <emrose@mozilla.com>
2019-02-15 18:57:42 -0500

    Implemented both request handlers

--- patches/lib/patches/web_server.ex
+++ patches/lib/patches/web_server.ex
@@ -1,6 +1,9 @@
 defmodule Patches.WebServer do
   import Plug.Conn
 
+  alias Patches.Server.Agent, as: Sessions
+  alias Patches.StreamRegistry.Agent, as: VulnStreams
+
   @missing_params_msg "Request missing required query string parameters platform/requestID" 
 
   def init(options), do: options
@@ -23,14 +26,38 @@ defmodule Patches.WebServer do
       _ ->
         error(conn)
     end
-    
-    send_resp(conn, 200, "Hello, World")
   end
 
   defp serve_vulnerabilities(conn, req_id) do
+    {status, response} =
+      case VulnStreams.retrieve(req_id) do
+        [] ->
+          {202, %{ "error" => "There are no vulnerabilities prepared for your session."}}
+
+        vulns ->
+          {200, %{ "error" => nil, "vulnerabilities" => vulns }}
+      end
+
+    body =
+      Poison.encode!(response)
+
+    send_resp(conn, status, body)
   end
 
   defp register_session(conn, platform) do
+    {status, response} =
+      case Sessions.queue_session(scanning: platform) do
+        {:ok, new_session_id} ->
+          {200, %{ "error" => nil, "requestID" => new_session_id }}
+
+        {:error, :queue_full} ->
+          {202, %{ "error" => "The server is too busy right now. Try again later." }}
+      end
+
+    body =
+      Poison.encode!(response)
+
+    send_resp(conn, status, body)
   end
 
   defp error(conn) do
