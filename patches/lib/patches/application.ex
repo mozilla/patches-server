160ae404c9b12430006ccf671702d667b1c9c89c
Emma Rose <emrose@mozilla.com>
2019-02-19 16:43:57 -0500

    Add the Timeout.Agent to the main application's supervision tree

--- patches/lib/patches/application.ex
+++ patches/lib/patches/application.ex
@@ -1,10 +1,10 @@
 defmodule Patches.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
+  alias Patches.Timeout.Agent, as: Timeouts
+  alias Patches.Timeout.Agent.Config, as: TimeoutsConfig
   alias Patches.Server.Agent, as: Sessions
   alias Patches.Server.Agent.Config, as: SessionsConfig
   alias Patches.StreamRegistry.Agent, as: VulnStreams
@@ -15,10 +15,9 @@ defmodule Patches.Application do
       {Plug.Cowboy, scheme: :http, plug: Patches.WebServer, options: [port: 9001]},
       {Sessions, %SessionsConfig{}},
       {VulnStreams, %VulnStreamsConfig{}},
+      {Timeouts, %TimeoutsConfig{}},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Patches.Supervisor]
     Supervisor.start_link(children, opts)
   end
