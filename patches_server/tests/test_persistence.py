7c4cd03fe2b88cade78a264c86996c363832c0c7
Emma Rose <emrose@mozilla.com>
2019-03-08 16:13:32 -0500

    Logging

--- patches_server/tests/test_persistence.py
+++ patches_server/tests/test_persistence.py
@@ -7,7 +7,7 @@ import requests
 from util import needs_patches_server, needs_redis
 
 
-@needs_patches_server
+#@needs_patches_server
 def test_handling_valid_sessions():
     print('in test_handling_valid_sessions', file=sys.stderr)
     session_id = requests.get('http://127.0.0.1:9002/?platform=ubuntu:18.04')\
--- patches_server/tests/util.py
+++ patches_server/tests/util.py
@@ -31,12 +31,17 @@ def needs_patches_server(test_fn, scheme='http', host='127.0.0.1', port=9002):
     Patches-Server before running a test.
     '''
 
+    print('Calling needs_patches_server', file=sys.stderr)
+
     def wrapper(*args, **kwargs):
         try:
             resp = requests.get(f'{scheme}://{host}:{port}?platform=none')
+            print('request succeeded', file=sys.stderr)
             if resp.json().get('error', None) is not None:
+                print('calling test fn', file=sys.stderr)
                 return test_fn(*args, **kwargs)
             
             return None
         except Exception:
+            print('request failed', file=sys.stderr)
             return None
