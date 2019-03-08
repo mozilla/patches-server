47b962d1af5a10c5e5ae301545d5197f41f0fc23
Emma Rose <emrose@mozilla.com>
2019-03-08 17:33:45 -0500

    Set up a test client fixture so we can probe the API

---
+++ patches_server/tests/test_api.py
@@ -0,0 +1,15 @@
+import pytest
+
+from patches_server.api import api
+
+
+@pytest.fixture
+def client():
+    api.config['TESTING'] = True
+
+    yield api.test_client()
+
+
+def test_hello(client):
+    response = client.get('/').json
+    assert response.get('greeting') == 'Hello, world!'
