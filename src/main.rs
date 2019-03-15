2329e6737b311445ce3991d5782cd436c99d335c
Emma Rose <emrose@mozilla.com>
2019-03-15 18:23:42 -0400

    Got actix_web to handle state between requests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 #[macro_use] extern crate serde_derive;
 
-use actix_web::{http, server, App, Query, Responder};
+use std::sync::{Arc, Mutex};
 
+use actix_web::{http, server, App, State, Query, Responder};
+
+
+#[derive(Clone)]
+struct AppState {
+  sessions: Arc<Mutex<u32>>
+}
 
 #[derive(Deserialize)]
 struct SessionRequest {
@@ -13,17 +20,28 @@ struct VulnsRequest {
   session: String,
 }
 
-fn session(query: Query<SessionRequest>) -> String {
-  format!("New session created for scanner on {}", query.platform)
+fn session(state: State<AppState>, query: Query<SessionRequest>) -> impl Responder{
+  *(state.sessions.lock().unwrap()) += 1;
+
+  let now_serving = state.sessions.lock().unwrap();
+
+  format!("New session created for scanner on {}. Now serving {} sessions",
+    query.platform, now_serving)
 }
 
-fn vulnerabilities(query: Query<VulnsRequest>) -> String {
+fn vulnerabilities(_state: State<AppState>, query: Query<VulnsRequest>) -> String {
   format!("Fetching vulns for session {}", query.session)
 }
 
 fn main() {
-  let server = server::new(|| {
-    App::new()
+  let sessions = Arc::new(Mutex::new(0));
+
+  let server = server::new(move || {
+    let state = AppState {
+      sessions: sessions.clone(),
+    };
+
+    App::with_state(state)
       .resource("/vulnerabilities", |r| r.method(http::Method::GET).with(session))
       .resource("/vulnerabilities/poll", |r| r.method(http::Method::GET).with(vulnerabilities))
   });
