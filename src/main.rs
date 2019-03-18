eb42e161b5aa6437a73b8ec2e543d1a661c38f26
Emma Rose <emrose@mozilla.com>
2019-03-18 15:53:49 -0400

    Working on adding support for state persistence

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate redis_async;
 
 mod persist;
 
 use std::sync::{Arc, Mutex};
 
-use actix_redis::RedisActor;
-use actix_web::{http, server, App, State, Query, Responder};
+use actix_redis::{RedisActor, Command};
+use actix_web::{http, server, Addr, App, State, Query, Responder};
 
 use persist::{Persistent, Redis};
 
+
+#[derive(Clone)]
+struct State {
+  application: AppState,
+  configuration: Config,
+  redis: Addr<RedisActor>,
+}
+
+struct Config {
+}
 
 #[derive(Clone)]
 struct AppState {
@@ -38,6 +49,8 @@ fn vulnerabilities(_state: State<AppState>, query: Query<VulnsRequest>) -> Strin
   format!("Fetching vulns for session {}", query.session)
 }
 
+fn unit1<T>(_any: T) -> () { () }
+
 fn main() {
   let sessions = Arc::new(Mutex::new(0));
 
@@ -61,8 +74,35 @@ impl Persist<Addr<RedisActor>> for AppState {
   type Error = {};
 
   fn persist(&self, redis_actor: &Addr<RedisActor>) -> Result<(), ()> {
+    let num_sessions = format!("{}", self.sessions);
+
+    redis_actor
+      .try_send(Command(resp_array!["SET", "sessions", num_sessions]))
+      .map_err(unit1)
+      .map(unit1)
   }
 
   fn rebuild(redis_actor: &Addr<RedisActor>) -> Result<AppState, ()> {
+    let response = redis_actor
+      .try_send(Command(resp_array!["GET", "sessions"]))
+      .map_err(unit1);
+
+    match response {
+      Ok(RespValue::SimpleString(value)) => {
+        let num_sessions = value.parse::<u32>().map_err(unit1)?;
+
+        Ok(AppState{ sessions: num_sessions })
+      },
+
+      Ok(RespValue::BulkString(bytes)) => {
+        let num_sessions = String::from_utf8(bytes)
+          .and_then(|string| string.parse::<u32>().map_err(unit1))
+          .map_err(unit1)?;
+
+        Ok(AppState{ sessions: num_sessions })
+      },
+
+      _ => Err(()),
+    }
   }
 }
